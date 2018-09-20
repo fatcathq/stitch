0f8b9e9eb393aa83d1c89f87f3898f21d48b5602
Dimitris Lamprinos <pkakelas@gmail.com>
2018-09-20 19:47:35 +0300

    Add helpers functions

---
+++ helpers.ts
@@ -0,0 +1,33 @@
+import { Triangle, Market } from './types'
+import EdgeDriver from './edge'
+import * as _ from 'lodash'
+
+function triangleEquals (triangleA: Triangle, triangleB: Triangle): boolean {
+  const nodesA = triangleA.map(a => a.source)
+  const nodesB = triangleB.map(a => a.source)
+
+  return _.isEmpty(_.difference(nodesA, nodesB))
+}
+
+export function triangleExists (candidate: Triangle, triangles: Triangle[]): boolean {
+  for (let triangle of triangles) {
+    if (triangleEquals(candidate, triangle)) {
+      return true
+    }
+  }
+  return false
+}
+
+export function marketIsValid (market: Market): boolean {
+  // Filter for inconsistent results. Happens mainly in binance
+  return !/e/.test(String(market.bid))
+    && !/e/.test(String(market.ask))
+    && /([A-Z]{2,5}\/[A-Z]{2,5})/.test(market.symbol)
+    && market.bid !== 0
+    && market.ask !== 0
+}
+
+//Calculate arbitrage by getting the product of the (after fee) edge weights  
+export function calculateArbitrage (triangle: Triangle, fee: number): number {
+  return triangle.map((e: EdgeDriver) => e.getWeight()).reduce((acc, v) => acc * (1 - fee) * v, 1)
+}
