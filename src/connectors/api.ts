504b4e8c6d256d2070408e528ee311d73728a29d
Dimitris Lamprinos <pkakelas@gmail.com>
2018-10-05 18:56:55 +0300

    Log every ccxt request

--- src/connectors/api.ts
+++ src/connectors/api.ts
@@ -1,11 +1,20 @@
 const ccxt = require('ccxt')
 const mem = require('mem')
+const fetch = require('fetch-ponyfill')().fetch
+import log from '../loggers/winston'
 
 import config from  '../utils/config'
 
 export default class instance extends ccxt[config.exchange] {
   constructor() {
-    super({ apiKey: config.api.key, secret: config.api.secret })
+    super({ apiKey: config.api.key, secret: config.api.secret, enableRateLimit: true })
+    this.fetchImplementation = (url: string, ...args: any[]) => {
+      log.info(`[REQUEST] ${url}`)
+      if (args[0].body !== undefined) {
+        log.info(`[REQUEST] Body: ${args[0].body}`)
+      }
+      return fetch(url, ...args)
+    }
   }
 
   private _fetchOrderBook = mem(super.fetchOrderBook, { maxAge: config.cacheTime})
