495614f8fb7ad327cd6bf7ec80e078cd4e56da87
Dimitris Lamprinos <pkakelas@gmail.com>
2018-10-08 19:24:26 +0300

    Add custom rate limiting

--- src/connectors/api.ts
+++ src/connectors/api.ts
@@ -2,24 +2,35 @@ const ccxt = require('ccxt')
 const mem = require('mem')
 const fetch = require('fetch-ponyfill')().fetch
 import log from '../loggers/winston'
+const RateLimiter = require('stopcock')
 
 import config from  '../utils/config'
 
 export default class instance extends ccxt[config.exchange] {
   constructor() {
-    super({ apiKey: config.api.key, secret: config.api.secret, enableRateLimit: true })
-    this.fetchImplementation = (url: string, ...args: any[]) => {
-      log.info(`[REQUEST] ${url}`)
-      if (args[0].body !== undefined) {
-        log.info(`[REQUEST] Body: ${args[0].body}`)
-      }
-      return fetch(url, ...args)
-    }
+    super({ apiKey: config.api.key, secret: config.api.secret, timeout: 100000000 })
+
+    this.fetchImplementation = new RateLimiter(this.fetchWithCache, { limit: 1, interval: 1000, bucketSize: 1 })
   }
 
-  private _fetchOrderBook = mem(super.fetchOrderBook, { maxAge: config.cacheTime})
+  private _fetchOrderBook = mem(super.fetchOrderBook, { maxAge: config.cacheTime })
 
   public async fetchOrderBook (market: string, limit: number) {
     return this._fetchOrderBook(market, limit)
+  }
+
+  public emptyCache () {
+    log.info(`[CACHE] Emptying queue`)
+    this.fetchImplementation = new RateLimiter(this.fetchWithCache, { limit: 1, interval: 2000, bucketSize: 1 })
+  }
+
+  private async fetchWithCache (url: string, ...args: any[]) {
+    log.info(`[REQUEST] ${url}`)
+
+    if (args[0].body !== undefined) {
+      log.info(`[REQUEST] body: ${args[0].body}`)
+    }
+
+    return await fetch(url, ...args)
   }
 }
