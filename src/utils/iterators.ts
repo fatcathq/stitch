6817b625d88ae429ec5cfc3dcaff483fc85bd321
Dimitris Lamprinos <pkakelas@gmail.com>
2018-10-28 23:51:34 +0200

    Debug iterators

--- src/utils/iterators.ts
+++ src/utils/iterators.ts
@@ -1,15 +1,23 @@
 import Decimal from 'decimal.js'
 import { Edge, VirtualEdge } from '../models/edge'
 
+// Fees are applied in always in the target of the edge
 export function bitfinex(volume: Decimal, edge: Edge): Decimal {
-  return new Decimal(volume).mul(edge.getPriceAsDecimal()).mul((1 - edge.fee))
+  return volume.mul(edge.getPriceAsDecimal()).mul((1 - edge.fee))
 }
 
+/**
+ * Fees are applied always in the currency.
+ * - On sell position fees are applied to target
+ * - On bid position fees are applied on source
+ */
 export function bittrex(volume: Decimal, edge: Edge): Decimal {
   if (edge instanceof VirtualEdge) {
-    return new Decimal(volume).mul(edge.getPriceAsDecimal()).minus(edge.fee)
+    const afterFee = volume.mul(new Decimal(1).minus(edge.fee))
+
+    return afterFee.mul(edge.getPriceAsDecimal())
   }
   else {
-    return new Decimal(volume).mul(edge.getPriceAsDecimal()).mul(1 - edge.fee)
+    return volume.mul(edge.getPriceAsDecimal()).mul(1 - edge.fee)
   }
 }
