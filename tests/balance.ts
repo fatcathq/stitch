d0c755818219e1317bd839ef957faadbd9f0aaa0
Dimitris Lamprinos <pkakelas@gmail.com>
2018-12-03 21:36:41 +0200

    Add precision tests in balance tests

--- tests/balance.ts
+++ tests/balance.ts
@@ -17,16 +17,59 @@ const balanceToDecimalValues = (balance: any) => {
   return balance
 }
 
-test('Update', async () => {
+describe('update', async () => {
+  let balanceHandler: Balance
+  let mockAPI: any
   const testBalance = {
-    ETH: 2,
-    BTC: 0.2
+    ETH: 100,
+    BTC: 200,
+    USD: 300
   }
-  const mockAPI = createBalanceAPIMock(testBalance)
 
-  const balanceHandler = new Balance(mockAPI)
-  await balanceHandler.update()
+  beforeAll(async () => {
+    mockAPI = createBalanceAPIMock(testBalance)
+    balanceHandler = new Balance(mockAPI)
+    await balanceHandler.update()
+  })
 
-  expect(balanceHandler.balance).toEqual(balanceToDecimalValues(testBalance))
-  expect(mockAPI.fetchBalance).toHaveBeenCalledTimes(1)
+  test('Balance handler should update balance obj properly', async () => {
+    expect(balanceHandler.balance).toEqual(balanceToDecimalValues(testBalance))
+  })
+
+  test('Balance handler should call api.fetchBalance once', async () => {
+    expect(mockAPI.fetchBalance).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe('precisions', async () => {
+  let balanceHandler: Balance
+  let mockAPI: any
+  const testBalance = {
+    ETH: 0.1234567,
+    BTC: 0.1234567,
+    USD: 0.1234567
+  }
+  const precisions = {
+    ETH: 2,
+    BTC: 10,
+  }
+
+  beforeAll(async () => {
+    mockAPI = createBalanceAPIMock(testBalance)
+    balanceHandler = new Balance(mockAPI)
+    await balanceHandler.setPrecisions(precisions)
+    await balanceHandler.update()
+  })
+
+  test('Balance handler should not cut decimals when there is non existing precision on that unit', () => {
+    expect(balanceHandler.balance.USD.toNumber()).toEqual(testBalance.USD)
+  })
+
+  test('Balance handler should not cut decimals when precision is bigger than floating points', () => {
+    expect(balanceHandler.balance.BTC.toNumber()).toEqual(Number(testBalance.BTC.toPrecision(precisions.BTC)))
+  })
+
+  test('Balance handler should cut decimals properly', () => {
+    expect(balanceHandler.balance.ETH.toNumber()).toEqual(Number(testBalance.ETH.toPrecision(precisions.ETH)))
+  })
 })
