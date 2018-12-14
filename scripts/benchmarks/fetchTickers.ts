895c04f8565ea14676c2e4f2bfc65c78b1eae1d8
Dimitris Lamprinos <pkakelas@gmail.com>
2018-12-14 15:30:24 +0200

    Add fetchTickers

---
+++ scripts/benchmarks/fetchTickers.ts
@@ -0,0 +1,24 @@
+import benchmark from './base'
+import * as dotenv from 'dotenv'
+
+(async () => {
+  const ENV_CONFIG = './.env'
+  dotenv.config({path: ENV_CONFIG})
+
+  const {EXCHANGE, SAMPLES, API_KEY, API_SECRET} = process.env
+
+  if (!EXCHANGE || !SAMPLES || !API_KEY || !API_SECRET) {
+    console.log('Env vars are missing from .env')
+    return
+  }
+
+  await benchmark({
+    exchange: 'bittrex',
+    method: 'fetchTickers',
+    samples: Number(SAMPLES),
+    api: {
+      key: API_KEY,
+      secret: API_SECRET
+    }
+  })
+})()
