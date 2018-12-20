711c6058da1a7a3cee951489cb47881dd00aa640
Dionysis Zindros <dionyziz@gmail.com>
2018-12-20 22:18:45 +0200

    Create some basic edge tests

---
+++ tests/models/edge.ts
@@ -0,0 +1,58 @@
+import { Edge, VirtualEdge } from '../../src/models/edge'
+import Decimal from 'decimal.js'
+
+const createOrderBookAPIMock = (): any => {
+  return {
+    fetchOrderBook: jest.fn().mockReturnValue({
+      bids: [
+        [5.1, 9], // price, volume
+        [4.9, 12]
+      ],
+      asks: [
+        [5.2, 8.6],
+        [5.6, 0.01]
+      ]
+    })
+  }
+}
+
+describe(
+  'edges', async () => {
+    test('Edges have markets', async () => {
+      let edge = new Edge('ADA', 'BTC', [new Decimal(0), 'before'], new Decimal(0))
+
+      expect(edge.getMarket()).toBe('ADA/BTC')
+
+      let virtualEdge = new VirtualEdge('BTC', 'ADA', [new Decimal(0), 'before'], new Decimal(0))
+
+      expect(virtualEdge.getMarket()).toBe('ADA/BTC')
+    })
+
+    test('Edges have prices', async () => {
+      let edge = new Edge('ADA', 'BTC', [new Decimal(0), 'before'], new Decimal(0))
+
+      edge.setPrice(new Decimal(3.14159265))
+      expect(edge.getPrice().toNumber()).toBeCloseTo(3.14159265)
+
+      edge.setRealPrice(new Decimal(2.71828))
+      expect(edge.getPrice().toNumber()).toBeCloseTo(2.71828)
+
+      let virtualEdge = new VirtualEdge('BTC', 'ADA', [new Decimal(0), 'before'], new Decimal(0))
+
+      virtualEdge.setPrice(new Decimal(3))
+      expect(virtualEdge.getPrice().toNumber()).toBeCloseTo(3)
+
+      virtualEdge.setRealPrice(new Decimal(5))
+      expect(virtualEdge.getPrice().toNumber()).toBeCloseTo(1/5)
+    })
+
+    test('Edges can update from API', async () => {
+      let api = createOrderBookAPIMock()
+      let edge = new Edge('ADA', 'BTC', [new Decimal(0), 'before'], new Decimal(0))
+
+      await edge.updateFromAPI(api)
+
+      expect(edge.getVolume().toNumber()).toBe(9)
+    })
+  }
+)
