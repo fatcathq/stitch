7cf9ceb3d0f31a3fa2798f2205d741207969ad14
Dionysis Zindros <dionyziz@gmail.com>
2018-12-20 22:18:32 +0200

    Remove unnecessary edge default values from test

--- tests/models/opportunity.ts
+++ tests/models/opportunity.ts
@@ -5,13 +5,13 @@ import Opportunity from '../../src/models/opportunity'
 describe(
   'calculateArbitrage', async () => {
     test('No-fee arbitrage is calculated correctly for real edges', async () => {
-      const edge1 = new Edge('BTC', 'ADA', [new Decimal(0), 'before'], new Decimal(0), [8, 8])
+      const edge1 = new Edge('BTC', 'ADA', [new Decimal(0), 'before'], new Decimal(0))
       edge1.setPrice(new Decimal(1))
 
-      const edge2 = new Edge('ADA', 'USD', [new Decimal(0), 'after'], new Decimal(0), [8, 8])
+      const edge2 = new Edge('ADA', 'USD', [new Decimal(0), 'after'], new Decimal(0))
       edge2.setPrice(new Decimal(2))
 
-      const edge3 = new Edge('USD', 'BTC', [new Decimal(0), 'before'], new Decimal(0), [8, 8])
+      const edge3 = new Edge('USD', 'BTC', [new Decimal(0), 'before'], new Decimal(0))
       edge3.setPrice(new Decimal(3))
 
       const p = new Opportunity('testExchange', [edge1, edge2, edge3])
@@ -21,13 +21,13 @@ describe(
     })
 
     test('No-fee arbitrage is calculated correctly', async () => {
-      const edge1 = new VirtualEdge('BTC', 'ADA', [new Decimal(0), 'before'], new Decimal(0), [8, 8])
+      const edge1 = new VirtualEdge('BTC', 'ADA', [new Decimal(0), 'before'], new Decimal(0))
       edge1.setPrice(new Decimal(1))
 
-      const edge2 = new Edge('ADA', 'USD', [new Decimal(0), 'after'], new Decimal(0), [8, 8])
+      const edge2 = new Edge('ADA', 'USD', [new Decimal(0), 'after'], new Decimal(0))
       edge2.setPrice(new Decimal(2))
 
-      const edge3 = new VirtualEdge('USD', 'BTC', [new Decimal(0), 'before'], new Decimal(0), [8, 8])
+      const edge3 = new VirtualEdge('USD', 'BTC', [new Decimal(0), 'before'], new Decimal(0))
       edge3.setPrice(new Decimal(3))
 
       const p = new Opportunity('testExchange', [edge1, edge2, edge3])
