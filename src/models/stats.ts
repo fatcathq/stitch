323e970d7f2b426d6486d74b6653a3913bd74625
Dimitris Lamprinos <pkakelas@gmail.com>
2019-02-14 18:56:24 +0200

    Add opportunities on stats logging

--- src/models/stats.ts
+++ src/models/stats.ts
@@ -32,9 +32,61 @@ export default class Stats {
 
   public init (): void {
     setInterval(() => {
-      const stats = this.logStats()
-      this.logBeautified(stats)
+      const edgeStats = this.getEdgeStats()
+      const opportunityStats = this.getOpportunityStats()
+      this.logBeautified(edgeStats, opportunityStats)
     }, LOG_INTERVAL)
+  }
+
+  public logBeautified (edgeStats: any, opportunityStats: any): void {
+    console.log(this.createBorder())
+    console.log(this.createEdgeLogsString(edgeStats))
+    console.log(this.createOpportunityLogsString(opportunityStats))
+    console.log(this.createBorder())
+  }
+
+  private getEdgeStats (): any {
+    const edgeSamples = this.getSamplesFromGraph()
+    const settedEdgeSamples = this.getSettedEdgeSamples(edgeSamples)
+    const lastUpdatedStats = this.getDescriptiveStatsBy(settedEdgeSamples, 'lastUpdated')
+    const updatedOnLastInterval = this.filterUpdatedOnLastInterval(settedEdgeSamples)
+
+    return {
+      settedEdgesLength: settedEdgeSamples.length,
+      updatedOnLastInterval: updatedOnLastInterval.length,
+      lastUpdated: lastUpdatedStats
+    }
+  }
+
+  private getOpportunityStats (): any {
+    return {
+      openOpportunitiesLen: Object.keys(this.opportunities).length
+    }
+  }
+
+  public createOpportunityLogsString (opportunityStats: any): string {
+    let str: string = ''
+
+    str += `| Open opportunities: ${opportunityStats.openOpportunitiesLen} \n`
+
+    return str
+  }
+
+  public createEdgeLogsString (stats: any): string {
+    let str: string = ''
+
+    str += `| All edges: ${this.graph.edges().length}. SettedEdges: ${stats.settedEdgesLength} \n`
+
+    if (stats.lastUpdated.max === undefined) {
+      return str
+    }
+
+    str += `| Edges updated on last interval: ${stats.updatedOnLastInterval} \n`
+    str += `| lastUpdated mean: ${stats.lastUpdated.mean} ms, std: ${stats.lastUpdated.std} \n`
+    str += `| lastUpdated min: ${stats.lastUpdated.min.lastUpdated} ms on ${stats.lastUpdated.min.source}->${stats.lastUpdated.min.target} with price: ${stats.lastUpdated.min.price}, realPirce: ${stats.lastUpdated.min.realPrice} and volume: ${stats.lastUpdated.min.volume} \n`
+    str += `| lastUpdated max ${stats.lastUpdated.max.lastUpdated} ms on ${stats.lastUpdated.max.source}->${stats.lastUpdated.max.target} with price: ${stats.lastUpdated.max.price}, realPirce: ${stats.lastUpdated.max.realPrice} and volume: ${stats.lastUpdated.max.volume} \n`
+
+    return str
   }
 
   private getSamplesFromGraph (graph: Graph = this.graph): EdgeSample[] {
@@ -77,42 +129,6 @@ export default class Stats {
     const avg = _.sum(array) / array.length
 
     return Math.sqrt(_.sum(_.map(array, (i) => Math.pow((i - avg), 2))) / array.length)
-  }
-
-  private logStats (): any {
-    const edgeSamples = this.getSamplesFromGraph()
-    const settedEdgeSamples = this.getSettedEdgeSamples(edgeSamples)
-    const lastUpdatedStats = this.getDescriptiveStatsBy(settedEdgeSamples, 'lastUpdated')
-    const updatedOnLastInterval = this.filterUpdatedOnLastInterval(settedEdgeSamples)
-
-    return {
-      settedEdgesLength: settedEdgeSamples.length,
-      updatedOnLastInterval: updatedOnLastInterval.length,
-      lastUpdated: lastUpdatedStats
-    }
-  }
-
-  public logBeautified (stats: any): void {
-    console.log(this.createBorder())
-    console.log(this.createEdgeLogsString(stats))
-    console.log(this.createBorder())
-  }
-
-  public createEdgeLogsString (stats: any): string {
-    let str: string = ''
-
-    str += `| All edges: ${this.graph.edges().length}. SettedEdges: ${stats.settedEdgesLength} \n`
-
-    if (stats.lastUpdated.max === undefined) {
-      return str
-    }
-
-    str += `| Edges updated on last interval: ${stats.updatedOnLastInterval} \n`
-    str += `| lastUpdated mean: ${stats.lastUpdated.mean} ms, std: ${stats.lastUpdated.std} \n`
-    str += `| lastUpdated min: ${stats.lastUpdated.min.lastUpdated} ms on ${stats.lastUpdated.min.source}->${stats.lastUpdated.min.target} with price: ${stats.lastUpdated.min.price}, realPirce: ${stats.lastUpdated.min.realPrice} and volume: ${stats.lastUpdated.min.volume} \n`
-    str += `| lastUpdated max ${stats.lastUpdated.max.lastUpdated} ms on ${stats.lastUpdated.max.source}->${stats.lastUpdated.max.target} with price: ${stats.lastUpdated.max.price}, realPirce: ${stats.lastUpdated.max.realPrice} and volume: ${stats.lastUpdated.max.volume} \n`
-
-    return str
   }
 
   private createBorder (): string {
