d4493a0721f7ff24ce34ee785c14f8ecdd9c03e1
Dimitris Lamprinos <pkakelas@gmail.com>
2019-04-24 18:27:08 +0300

    [BENCHMARKS] Make API_SECRET an optional

--- scripts/benchmarks/fetchBalance.ts
+++ scripts/benchmarks/fetchBalance.ts
@@ -1,24 +1,26 @@
-import benchmark from './base'
+import benchmark, { CCXTCreds } from './base'
 import * as dotenv from 'dotenv'
 
 (async () => {
   const ENV_CONFIG = './.env'
-  dotenv.config({path: ENV_CONFIG})
+  dotenv.config({ path: ENV_CONFIG })
 
-  const {EXCHANGE, SAMPLES, API_KEY, API_SECRET} = process.env
+  const { EXCHANGE, SAMPLES, API_KEY, API_SECRET } = process.env
 
-  if (!EXCHANGE || !SAMPLES || !API_KEY || !API_SECRET) {
+  if (!EXCHANGE || !SAMPLES || !API_KEY) {
     console.log('Env vars are missing from .env')
     return
+  }
+
+  const opts: CCXTCreds = { key: API_KEY }
+  if (API_SECRET) {
+    opts.secret = API_SECRET
   }
 
   await benchmark({
     exchange: EXCHANGE,
     method: 'fetchBalance',
     samples: Number(SAMPLES),
-    api: {
-      key: API_KEY,
-      secret: API_SECRET
-    }
+    api: opts
   })
 })()
