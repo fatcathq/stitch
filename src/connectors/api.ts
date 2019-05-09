6a2767e04e0d8927d806204ee637b487778e0fc4
Dimitris Lamprinos <pkakelas@gmail.com>
2019-05-09 18:21:14 +0300

    3 tries on failed requests

--- src/connectors/api.ts
+++ src/connectors/api.ts
@@ -15,11 +15,23 @@ export default class extends ccxt[config.exchange] {
 
   private async fetchWithLog (url: string, ...args: any[]): Promise<any> {
     const now = Date.now()
-    const res = await fetch(url, ...args)
+    let failed = 0
+    let done = false
+    let res: any
 
-    // Log in case of fetchOrder
-    if (url.includes('trading/orders')) {
-      log.info(`[REQUEST] ${url}, duration: ${Date.now() - now} ms, args: ${JSON.stringify(args)}`)
+    do {
+      res = await fetch(url, ...args)
+
+      if (!res.ok) {
+        failed++
+        log.error(`[REQUEST_FAILED] ${url}, duration: ${Date.now() - now} ms, args: ${JSON.stringify(args)}. ${failed < 3 ? 'Trying again' : ''}.`)
+      } else {
+        done = true
+      }
+    } while (!done && failed < 3)
+
+    if (!done) {
+      throw new APIError(url, res.statusText, args)
     }
 
     return res
